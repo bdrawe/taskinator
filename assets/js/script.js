4af93101ae71bf3f7c4d2eb066b426148adf6525
B. Drawe <brycerdrawe.b@gmail.com>
2020-08-17 22:06:22 -0600

    added comments and now the list items will display the text content and also the different types of categories

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,9 +4,21 @@ let tasksToDoEl = document.querySelector('#tasks-to-do');
 
 let createTaskHandler = function(event) {
     event.preventDefault();
+    let taskNameInput = document.querySelector("input[name='task-name']").value;
+    let taskTypeInput = document.querySelector("select[name='task-type']").value;
+    //create a list item
     let listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task";
+    //create a div to hold class info and add to list item
+    let taskInfoEl =  document.createElement("div");
+    //give it a class name
+    taskInfoEl.className = "task.info";
+    //add html to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "<h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+    listItemEl.appendChild(taskInfoEl);
+
+    //add entire list items to the list
     tasksToDoEl.appendChild(listItemEl);
 };
 
