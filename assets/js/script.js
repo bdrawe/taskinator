25aea0e4a948b98746eeabc638d5406983615230
B. Drawe <brycerdrawe.b@gmail.com>
2020-08-19 00:12:35 -0600

    status changing capabilities

--- assets/js/script.js
+++ assets/js/script.js
@@ -148,8 +148,27 @@ let taskButtonHandler = function(event) {
     }
 }
 
+
 let taskStatusChangeHandler = function(event) {
-    console.log(event.target);
+        // get the task item's id
+        let taskId = event.target.getAttribute("data-task-id");
+      
+        // get the currently selected option's value and convert to lowercase
+        let statusValue = event.target.value.toLowerCase();
+      
+        // find the parent task item element based on the id
+        let taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+    
+
+    if (statusValue === "to do") {
+        tasksToDoEl.appendChild(taskSelected);
+      } 
+      else if (statusValue === "in progress") {
+        tasksInProgressEl.appendChild(taskSelected);
+      } 
+      else if (statusValue === "completed") {
+        tasksCompletedEl.appendChild(taskSelected);
+      }
 };
 
 
